17d58cf454850f15cc256f68388d528948b4f637
caiyunwu_iwm <caiyunwu@iwaimai.baidu.com>
2017-02-06 11:32:43 +0800

    logger Test

---
+++ src/main/java/com/yun/common/ThreadTest.java
@@ -0,0 +1,14 @@
+package com.yun.common;
+
+/**
+ * Created by iWM on 2017/2/3.
+ */
+public class ThreadTest {
+    public int add(int a,int b){
+        return  a+b;
+    }
+
+    public int multi(int a,int b){
+        return a*b;
+    }
+}
